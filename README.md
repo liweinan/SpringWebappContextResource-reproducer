ab6f659694d6fcddded3b73390cd485d234f385b
阿男 <l.weinan@gmail.com>
2022-11-23 01:50:27 +0800

    init

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-reference: https://github.com/liweinan/RESTEasyLinksDemo/blob/master/pom.xml
-
-support CDI beans: https://docs.jboss.org/weld/reference/3.0.4.Final/en-US/html/environments.html
-
-https://www.baeldung.com/deploy-to-jetty
-
-Client-Side Filter: https://docs.jboss.org/resteasy/docs/4.6.0.Final/userguide/html_single/index.html#d4e2338
-
-- [JAX-RS - Client Filters](https://www.logicbig.com/tutorials/java-ee-tutorial/jax-rs/client-filters.html)# SpringWebappContextResource-reproducer
+```bash
+➤ curl http://localhost:8080/SpringWebappContextResource-reproducer/my/echo/uri
+http://localhost:8080/SpringWebappContextResource-reproducer/my/echo/uri⏎
+```
---
+++ src/main/java/io/weli/SpringWebappContextResource.java
@@ -0,0 +1,61 @@
+package io.weli;
+
+import org.jboss.logging.Logger;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.UriInfo;
+
+@Path("/echo")
+@Produces(MediaType.TEXT_PLAIN)
+public class SpringWebappContextResource {
+
+    private static Logger logger = Logger.getLogger(SpringWebappContextResource.class);
+    private UriInfo ui;
+    private HttpHeaders headers;
+
+    @Context
+    public void setUriInfo(UriInfo ui) {
+        this.ui = ui;
+    }
+
+    @Context
+    public void setHttpHeaders(HttpHeaders headers) {
+        this.headers = headers;
+    }
+
+    @GET
+    @Path("/uri")
+    public String echoURI() {
+        sleep();
+        logger.info("requestUribuilder: " + ui.getRequestUriBuilder().toString());
+        logger.info("request uri: " + ui.getRequestUri());
+        return (ui != null ? ui.getRequestUriBuilder().build().toString() : "null");
+    }
+
+    @GET
+    @Path("/headers")
+    public String echoHeaders() {
+        sleep();
+        logger.info("uri: " + ui.getRequestUri().toString());
+        logger.info("builder: " + ui.getRequestUriBuilder().build().toString());
+        String s = headers != null && headers.getRequestHeader(HttpHeaders.ACCEPT) != null ? (ui != null ? ui
+                .getRequestUriBuilder().build().toString()
+                : "null")
+                + ":" + headers.getRequestHeader(HttpHeaders.ACCEPT).get(0)
+                : "null";
+        logger.info("return: " + s);
+        return s;
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep((int) (Math.random() * 1000));
+        } catch (Exception e) {
+        }
+    }
+}
